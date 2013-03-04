b4699b56164e211c76c724297c85785d01723094
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2013-03-04 14:21:44 -0800

    Fix map/menu overlap issues

--- app/js/oscar/Loader.js
+++ app/js/oscar/Loader.js
@@ -227,6 +227,10 @@ var OscarLoader = function(base) {
  */
 var _OscarLoader = new OscarLoader(host.location);
 _OscarLoader.addResource( {
+	location :"../jquery/js/jquery.min.js",
+	type :"js"
+});
+_OscarLoader.addResource( {
 	location :"../jquery/css/smoothness/jquery-ui-1.8.16.custom.css",
 	type :"css"
 });
