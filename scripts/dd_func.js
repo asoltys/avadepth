335379eeb6c6cee71a508c81d516bfb101a6ad7c
Matty Wong-Rose <mathieu.wong-rose@pwgsc-tpsgc.gc.ca>
2015-07-07 14:42:34 -0700

    error handler for failed api calls

--- scripts/dd_func.js
+++ scripts/dd_func.js
@@ -116,9 +116,7 @@ if(!(typeof avaIFaceJS === 'undefined')) {
         $('#flowType').val("UserDefined");
       }
       //TODO: Replace bottom line for production
-      return $.getJSON(getAPI(("/api/depths/calculate?date=" + ($('#date').val()) + "&") + ("chainage=" + ($('#chainage').val()) + "&") + ("flowRate=" + ($('#flowRate').val()) + "&") + ("flowType=" + ($('#flowType').val()) + "&") + ("width=" + ($('#width').val()) + "&") + ("sounding=" + ($('input[name=condition]:checked').val())),"api/depths/calculate.json"), function (data) {
-      //return $.getJSON(("/api/depths/calculate?date=" + ($('#date').val()) + "&") + ("chainage=" + ($('#chainage').val()) + "&") + ("flowRate=" + ($('#flowRate').val()) + "&") + ("flowType=" + ($('#flowType').val()) + "&") + ("width=" + ($('#width').val()) + "&") + ("sounding=" + ($('input[name=condition]:checked').val())), function(data) {
-      //return $.getJSON("api/depths/calculate.json", function (data) {
+      return $.getJSON(getAPI(("/api/depths/calcafafdulate?date=" + ($('#date').val()) + "&") + ("chainage=" + ($('#chainage').val()) + "&") + ("flowRate=" + ($('#flowRate').val()) + "&") + ("flowType=" + ($('#flowType').val()) + "&") + ("width=" + ($('#width').val()) + "&") + ("sounding=" + ($('input[name=condition]:checked').val())),"api/depths/calculate.json"), function (data) {
         var points = [];
         avaIFaceJS.dd_func.tableReport || (avaIFaceJS.dd_func.tableReport = $('#depths').dataTable({
           bPaginate: false,
@@ -175,7 +173,7 @@ if(!(typeof avaIFaceJS === 'undefined')) {
         avaIFaceJS.reportWindow.show();
         avaIFaceJS.dd_func.createGraph(points);
         return $('.spinner').hide();
-      });
+      }).fail(avadepth.util.apiFailureHandler);
     },
 
     // Callback function for form submission
